122cbddbd5481596c3a3d3a48a18ec1148e017e8
Shayan Mahtabi <shayan.mhtb@gmail.com>
2024-01-21 13:05:36 +0100

    Main.hs modified for RedditAnalysis

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,11 @@
+-- app/Main.hs
+
 module Main where
 
+import RedditAnalysis
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Fetching Reddit Data..."
+  fetchRedditData
+  putStrLn "Reddit Data fetched and saved to reddit_data.json"
